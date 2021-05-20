f044fe84aecaa20ff492d416758957497ad7048d
reazul7 <islamreazul7777@gmail.com>
2021-05-20 15:32:29 +0600

    load all data from covid-19 api

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import {fetchData} from './api/index';
 
 
 class App extends React.Component {
+  state = {
+    data: {},
+  }
 
   async componentDidMount() {
-    const data = await fetchData();
-    
-    console.log(data);
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData});
   }
 
   render() {
+    const { data } = this.state;
     return (
       <div className="container">
-         <Cards/>
+         <Cards data={data}/>
          <Chart/>
          <CountryPicker/>
       </div>
@@ -24,26 +27,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-
-// import './App.css';
-
-// import { Cards, Chart, CountryPicker } from './components';
-// import {fetchData} from './api/index';
-
-// function App() {
-//   async componentDidMount() {
-//     const data = await fetchData();
-//     console.log(data);
-//   }
-//   return (
-//     <div className="container">
-//        <Cards/>
-//        <Chart/>
-//        <CountryPicker/>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,25 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        const response = await axios.get(url);
-        return response;
+        const { data } = await axios.get(url);   // destructuring data
+
+        const modifiedData = {
+            confirmed: data.confirmed,
+            recovered: data.recovered,
+            deaths: data.deaths,
+            lastUpdate: data.lastUpdate,
+        }
+
+        return modifiedData;
+    } catch (error) {
+
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        const {data} = await axios.get(`${url}/daily`);
+        console.log(data);
     } catch (error) {
 
     }
