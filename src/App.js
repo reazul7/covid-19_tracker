588fc41fe05d546268c597cd20c2e7370e5802f3
reazul7 <islamreazul7777@gmail.com>
2021-05-20 12:05:37 +0600

    add covid-19 url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Cards, Chart, CountryPicker } from './components';
+import {fetchData} from './api/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+    
+    console.log(data);
+  }
+
+  render() {
+    return (
+      <div className="container">
+         <Cards/>
+         <Chart/>
+         <CountryPicker/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+// import './App.css';
+
+// import { Cards, Chart, CountryPicker } from './components';
+// import {fetchData} from './api/index';
+
+// function App() {
+//   async componentDidMount() {
+//     const data = await fetchData();
+//     console.log(data);
+//   }
+//   return (
+//     <div className="container">
+//        <Cards/>
+//        <Chart/>
+//        <CountryPicker/>
+//     </div>
+//   );
+// }
+
+// export default App;
