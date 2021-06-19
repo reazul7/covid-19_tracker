679a54875fb6207edd3c5034bee85ea483d3434a
reazul7 <islamreazul7777@gmail.com>
2021-06-19 11:24:07 +0600

    second time global value show fix

--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -19,7 +19,7 @@ const CountryPicker = ({handleCountryChange}) => {
         <div className="container pt-4">
             <FormControl>
                 <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-                    <option value="global">Global</option>
+                    <option value="">Global</option>
                     {
                         fetchedCountries.map((country, i) => <option value={country} key={i}>{country}</option>)
                     }
